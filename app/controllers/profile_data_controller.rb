536f341faaa1e0e92b0629d00ef68e55a2cfd553
Bohdan Vakh <Bohdanvah@gmail.com>
2023-12-02 16:06:45 +0200

    feat/change methods names

--- app/controllers/profile_data_controller.rb
+++ app/controllers/profile_data_controller.rb
@@ -2,19 +2,9 @@ class ProfileDataController < ApplicationController
   def new
   end
 
-  def lorem
-    binding.pry
-  end
-
-  def create
-
-    binding.pry
-
+  def show
     github_login = params[:github_login]
     @result = GithubApiService.get_github_data(github_login)
-
-    binding.pry
-
     puts @result
   end
 end
